b3cd421832661b6c11c0465a1de9e35d517de895
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2023-04-28 23:14:16 +0700

    updated header

--- src/data.js
+++ src/data.js
@@ -58,7 +58,7 @@ export const headerData = {
     },
   ],
   actions: [
-    { type: 'button', text: 'Download', href: 'https://github.com/onwidget/astrowind' }
+    { type: 'button', text: 'Log In', href: 'https://clients.keyback.io/login' }
   ],
 };
   
