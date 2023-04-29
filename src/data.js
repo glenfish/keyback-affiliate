1603951063e82233079e6c2c2463f7e957079c5d
Nicholas Fehlberg <glenfish@nicholasfehlberg.com>
2023-04-29 21:30:41 +0700

    updated navbar menu

--- src/data.js
+++ src/data.js
@@ -19,10 +19,10 @@ export const headerData = {
     //     },
     //   ],
     // },
-    {
-      text: 'Video',
-      href: getPermalink('/videos'),
-    },
+    // {
+    //   text: 'Video',
+    //   href: getPermalink('/videos'),
+    // },
     {
       text: 'Pricing',
       href: getPermalink('/pricing'),
