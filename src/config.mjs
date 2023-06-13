85eb9065d2fceb91964824aaa9e616d50c50ac0c
Dave <realdavelister@gmail.com>
2023-06-13 12:31:23 +0300

    Google Analytics added

--- src/config.mjs
+++ src/config.mjs
@@ -24,7 +24,7 @@ const CONFIG = {
     timeZone: 'UTC',
   }),
 
-  googleAnalyticsId: false, // or "G-XXXXXXXXXX",
+  googleAnalyticsId: true, 'G-YWY1G29QVR',
   googleSiteVerificationId: 'orcPxI47GSa-cRvY11tUe6iGg2IO_RPvnA1q95iEM3M',
 
   blog: {
